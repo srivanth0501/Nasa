e42ee0bcc75ecc19e2b0ba6e718c8dc6410fd162
srivanth0501 <59724832+srivanth0501@users.noreply.github.com>
2025-02-17 21:55:40 +0000

    Update Api.js

--- frontend/src/Services/Api.js
+++ frontend/src/Services/Api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000/api';
+const BASE_URL = 'https://nasa-frontend-gmnn.onrender.com';
 
 export const loginUser = async (email, password) => {
     try {
